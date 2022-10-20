5fda1b5c65003fba4961737d45f8e8073f334122
Nicholas Tom <99391779+NickTom7@users.noreply.github.com>
2022-10-20 03:10:51 -1000

    edt

--- app/imports/ui/components/MainImage.jsx
+++ app/imports/ui/components/MainImage.jsx
@@ -4,7 +4,7 @@ import { Image, Row, Col } from 'react-bootstrap';
 const MainImage = () => (
   <Row className="justify-content-md-center pt-3" id="image">
     <Col md="auto">
-      <Image fluid src="/images/grif" />
+      <Image fluid src="/images/griff" />
     </Col>
   </Row>
 );
